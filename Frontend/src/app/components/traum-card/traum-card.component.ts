215d0f9fe0bbfcbd15f03350b64f26b64874e22e
Aven <semrau.j@web.de>
2025-04-05 11:44:59 +0200

    implemented that you can see the traum-card on the starting page and you can delete your dreams

--- Frontend/src/app/components/traum-card/traum-card.component.ts
+++ Frontend/src/app/components/traum-card/traum-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,4 +13,10 @@ export class TraumCardComponent {
   @Input() inhalt: string = '';
   @Input() bild: string = '';
   @Input() datum: string = '';
+
+  @Output() delete = new EventEmitter<void>();
+
+  onDelete() {
+    this.delete.emit();
+  }
 }
