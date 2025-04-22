b305537cc531c978aa702d61b4e546fdac70772d
PascalVeraguth <pascal.veraguth@cyrotech.ch>
2025-04-22 18:18:09 +0200

    Add logging variable in SecurityConfiguration

--- Backend/src/main/java/com/example/Backend/security/SecurityConfiguration.java
+++ Backend/src/main/java/com/example/Backend/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.Backend.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -22,6 +24,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
